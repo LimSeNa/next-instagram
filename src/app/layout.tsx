424865359f4c75481d38b53ddcbfbe82a764e569
Sena Lim <tpsk8322@naver.com>
2024-02-07 17:54:53 +0900

    feat: 사이드바, 팔로잉바 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import AuthContext from "@/context/AuthContext";
+import SWRConfigContext from "@/context/SWRConfigContext";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -16,18 +17,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={openSans.className}>
-    <body className='w-full max-w-screen-xl overflow-auto mx-auto'>
-    <AuthContext>
-        <header className='sticky top-0 bg-white z-10 border-b'>
-            <Navbar/>
-        </header>
-        <main>
-            {children}
-        </main>
-    </AuthContext>
-    </body>
-    </html>
-  );
+    return (
+        <html lang="en" className={openSans.className}>
+        <body className='w-full max-w-screen-xl overflow-auto mx-auto'>
+        <AuthContext>
+            <header className='sticky top-0 bg-white z-10 border-b'>
+                <Navbar/>
+            </header>
+            <main className='w-full flex justify-center bg-neutral-50 min-h-full'>
+                <SWRConfigContext>
+                    {children}
+                </SWRConfigContext>
+            </main>
+        </AuthContext>
+        </body>
+        </html>
+    );
 }
---
+++ src/components/FollowingBar.tsx
@@ -0,0 +1,37 @@
+'use client';
+import useSWR from "swr";
+import {DetailUser} from "@/model/user";
+import Link from "next/link";
+import Avatar from "@/components/Avatar";
+import {SyncLoader} from "react-spinners";
+import ScrollBar from "@/components/ui/ScrollBar";
+
+export default function FollowingBar() {
+    const {data, isLoading: loading, error} = useSWR<DetailUser>('/api/me');
+    // const users = data?.following;
+    // const users = undefined;
+    const users = data?.following && [...data?.following, ...data?.following, ...data?.following];
+
+    return (
+        <section
+            className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
+            {loading
+                ? <SyncLoader size={8} color='red'/>
+                : (!users || users.length === 0) && <p>{`You don't have following.`}</p>
+            }
+            {users && users.length > 0 && (
+                <ScrollBar>
+                    {users.map(({image, username}) => (
+                        <Link className='flex flex-col items-center w-20'
+                              href={`/user/${username}`}
+                              key={username}
+                        >
+                            <Avatar image={image} highlight/>
+                            <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>{username}</p>
+                        </Link>
+                    ))}
+                </ScrollBar>
+            )}
+        </section>
+    );
+}
